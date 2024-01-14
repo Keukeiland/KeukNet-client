a31754f5c9a1ca73c33294ed63b4076d5478db71
Fizitzfux <66255957+fizitzfux@users.noreply.github.com>
2024-01-14 12:31:02 +0000

    Initial commit

---
+++ installer.py
@@ -0,0 +1,83 @@
+#!/usr/bin/env python
+# This Source Code Form is subject to the terms of the Mozilla Public
+# License, v. 2.0. If a copy of the MPL was not distributed with this
+# file, You can obtain one at http://mozilla.org/MPL/2.0/.
+
+# compile on linux with `sudo wine pyinstaller --onefile installer.py`
+# cant run properly in wine!!
+import os
+import sys
+from sys import exit
+
+# Config values
+HOST = "connect.keukeiland.nl"
+DOWNLOAD_ARGS = "--no-progress-meter --fail"
+OTHER_DESKTOP_FILES_DIR = "/usr/share/applications"
+OTHER_BINARIES_DIR = "/usr/bin"
+OTHER_DEPENDENCIES = {
+    'wg': "WireGuard",
+    'xdg-mime': "XDG",
+    'curl': "cURL",
+    'chmod': "chmod"
+}
+
+# Constants
+WINDOWS = os.name == 'nt'
+OTHER = not WINDOWS
+
+
+# Initialization
+if WINDOWS:
+    import ctypes
+    # Privileges
+    if not ctypes.windll.shell32.IsUserAnAdmin() == 1:
+        ctypes.windll.shell32.ShellExecuteW(None, "runas", sys.executable, " ".join(sys.argv), None, 1)
+        exit(1)
+    # Confirmation
+    os.system("mshta \"javascript:alert('Before continuing please save all open files.');close()\"")
+if OTHER:
+    # Privileges
+    if not os.geteuid() == 0:
+        print("Not running as root, cancelling install...")
+        exit(1)
+    # Confirmation
+    input(f"This script will install:\n'keuknet.desktop' to '{OTHER_DESKTOP_FILES_DIR}'\n'keuknet.py' to '{OTHER_BINARIES_DIR}'\nPress 'CTRL+C' to cancel or 'Enter/Return' to continue.")
+
+
+# Preparation
+if WINDOWS:
+    os.system("mkdir \"%TEMP%\\keuknetinstaller\"")
+    os.system(f"curl -o \"%TEMP%\\keuknetinstaller\\wireguard.exe\" https://download.wireguard.com/windows-client/wireguard-installer.exe {DOWNLOAD_ARGS}")
+    os.system(f"curl -o \"%TEMP%\\keuknetinstaller\\keuknet.exe\" https://{HOST}/assets/keuknet.exe {DOWNLOAD_ARGS}")
+    os.system("mkdir \"%PROGRAMFILES%\\KeukNet\"")
+if OTHER:
+    if not os.path.isdir(OTHER_DESKTOP_FILES_DIR) or not os.path.isdir(OTHER_BINARIES_DIR):
+        print("Unusual file-system lay-out detected. Is your OS UNIX-compliant?\nIf not please edit this script to use the correct paths.")
+        exit(1)
+    for cmd, name in OTHER_DEPENDENCIES.items():
+        if os.system(f"{cmd} --version >/dev/null") != 0:
+            print(f"{name} not available at '{cmd}'. Please install a package that provides {cmd}.")
+            exit(1)
+
+
+# Installation
+if WINDOWS:
+    os.system("%TEMP%\\keuknetinstaller\\wireguard.exe && taskkill /IM wireguard.exe /F")
+    os.system("move \"%TEMP%\\keuknetinstaller\\keuknet.exe\" \"%PROGRAMFILES%\\KeukNet\\keuknet.exe\"")
+    os.system("reg add HKCR\\keuknet /ve /d \"URL:KeukNet\"  /f")
+    os.system("reg add HKCR\\keuknet /v \"URL Protocol\" /d "" /f")
+    os.system("reg add HKCR\\keuknet\\shell /f")
+    os.system("reg add HKCR\\keuknet\\shell\\open /f")
+    os.system("reg add HKCR\\keuknet\\shell\\open\\command /d \"\\\"C:\\Program Files\\KeukNet\\keuknet.exe\\\" \"%1\"\" /f")
+if OTHER:
+    os.system(f"curl -o \"{OTHER_DESKTOP_FILES_DIR}/keuknet.desktop\" https:/{HOST}/assets/keuknet.desktop {DOWNLOAD_ARGS}")
+    os.system(f"curl -o \"{OTHER_BINARIES_DIR}/keuknet.py\" https:/{HOST}/assets/keuknet.py {DOWNLOAD_ARGS}")
+    os.system(f"chmod +x \"{OTHER_BINARIES_DIR}/keuknet.py\"")
+    os.system("xdg-mime default keuknet.desktop x-scheme-handler/keuknet")
+
+
+# Finalization
+if WINDOWS:
+    os.system("shutdown /g /f /t 3 /d p:4:2 /c \"Rebooting in 3 sec to finish KeukNet installation\"")
+if OTHER:
+    print("Successfully installed the KeukNet client.")
---
+++ keuknet.py
@@ -0,0 +1,111 @@
+#!/usr/bin/env python
+# This Source Code Form is subject to the terms of the Mozilla Public
+# License, v. 2.0. If a copy of the MPL was not distributed with this
+# file, You can obtain one at http://mozilla.org/MPL/2.0/.
+
+# compile on linux with `sudo wine pyinstaller --onefile keuknet.py`
+# cant run properly in wine!!
+import os
+import sys
+import subprocess
+from sys import argv as args
+from sys import exit
+
+DEBUG = True
+HOST = "connect.keukeiland.nl"
+DOWNLOAD_ARGS = "--no-progress-meter --fail"
+
+# Constants
+WINDOWS = os.name == 'nt'
+OTHER = not WINDOWS
+
+if OTHER and not os.geteuid() == 0:
+    import notify2
+    notify2.init("KeukNet client")
+    def notify(content):
+        notify2.Notification('KeukNet',content).show()
+else:
+    import builtins as __builtin__
+    def notify(content):
+        __builtin__.print(content)
+
+
+def print(*args):
+    if DEBUG:
+        for arg in args:
+            notify(str(arg))
+    else:
+        pass
+
+def run(command):
+    try:
+        subprocess.run(command, shell=True, check=True, capture_output=True)
+    except subprocess.CalledProcessError as err:
+        print(f"error when running command'{command}'!")
+        if WINDOWS:
+            os.system(f"echo \"{err} {err.stderr.decode('utf8')}\" >> \"%TEMP%\\keuknet.log\"")
+        if OTHER:
+            os.system(f"echo \"{err}: {err.stderr.decode('utf8')}\" >>/tmp/keuknet.log")
+        exit(1)
+    
+
+def admin():
+    """Obtain admin privileges
+    """
+    if WINDOWS:
+        import ctypes
+        if not ctypes.windll.shell32.IsUserAnAdmin() == 1:
+            ctypes.windll.shell32.ShellExecuteW(None, "runas", sys.executable, " ".join(sys.argv), None, 1)
+            exit(0)
+    if OTHER:
+        if not os.geteuid() == 0:
+            print("Script not started as root. Running sudo..")
+            run(f"sudo {sys.executable} {' '.join(sys.argv)}")
+            exit(0)
+
+if WINDOWS:
+    os.system(f"echo \">{args}\" >> \"%TEMP%\\keuknet.log\"")
+
+# Check if valid uri
+if not args[-1].startswith("keuknet://"):
+    print("Called with incorrect or missing protocol")
+    exit(1)
+
+args = args[-1]
+command = args.split("keuknet://",1)[1]
+instruction, args = command.split("=") if "=" in command else [command,""]
+instruction = instruction.split("&")[0] if "&" in instruction else instruction
+instruction = instruction.split(";")[0] if ";" in instruction else instruction
+args = args.split("&")[0] if "&" in args else args
+args = args.split(";")[0] if ";" in args else args
+args = args.split("/")[0] if "/" in args else args
+print(command, instruction, args)
+
+if WINDOWS:
+    os.system(f"echo \">{instruction}!!{args}\" >> \"%TEMP%\\keuknet.log\"")
+
+match instruction:
+    case "install":
+        notify("Installing new profile...")
+        admin()
+        if WINDOWS:
+            os.system("del /F \"%PROGRAMFILES%\\WireGuard\\Data\\Configurations\\keuknet.conf.dpapi\"")
+            run(f"curl -o \"%PROGRAMFILES%\\WireGuard\\Data\\Configurations\\keuknet.conf\" \"https://{HOST}/profile/getconf?{args}\" {DOWNLOAD_ARGS}")
+        if OTHER:
+            run(f"curl -o \"/etc/wireguard/keuknet.conf\" https://{HOST}/profile/getconf?{args} {DOWNLOAD_ARGS}")
+    case "load":
+        notify("Enabling keuknet...")
+        admin()
+        if WINDOWS:
+            run("\"%PROGRAMFILES%\\WireGuard\\wireguard.exe\" /installtunnelservice \"%PROGRAMFILES%\\WireGuard\\Data\\Configurations\\keuknet.conf.dpapi\"")
+        if OTHER:
+            run("wg-quick up keuknet")
+    case "unload":
+        notify("Disabling keuknet...")
+        admin()
+        if WINDOWS:
+            run("\"%PROGRAMFILES%\\WireGuard\\wireguard.exe\" /uninstalltunnelservice keuknet >nul")
+        if OTHER:
+            run("wg-quick down keuknet")
+    case _:
+        print("Invalid instruction")
